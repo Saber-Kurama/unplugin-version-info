7ece65b29d04420427ae179ebb3ee63496f2d8ba
guochao <463561248@qq.com>
2023-04-12 20:24:21 +0800

    ✨ feat:  添加监听build

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,29 @@ import { createUnplugin } from "unplugin";
 import type { Options } from "./types";
 import { getConsoleLogString } from "./utils/log";
 
+let isWebpack = false;
+let isBuild = false;
+
 export default createUnplugin<Options | undefined>((options) => ({
   name: "unplugin-version-info",
   transformInclude(id) {
     return id.endsWith(options?.filename || "index.html");
   },
   transform(code) {
-    console.log("this", this);
+    if (isWebpack && !isBuild) {
+      return code;
+    }
     return code.replace("</head>", `${getConsoleLogString()}</head>`);
+  },
+  vite: {
+    apply(_, { command }) {
+      if (command === "serve") return false;
+      if (command === "build") return true;
+      return false;
+    },
+  },
+  webpack(compiler) {
+    isWebpack = true;
+    isBuild = process.argv.includes("build");
   },
 }));
